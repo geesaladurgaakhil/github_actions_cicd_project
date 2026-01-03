4c22093564c206ad491631b9d80b72fb6680912b
geesaladurgaakhil <56544604+geesaladurgaakhil@users.noreply.github.com>
2026-01-03 16:51:23 +0530

    Added Source Code

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("🚀 Node.js app deployed to AKS using GitHub Actions!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
